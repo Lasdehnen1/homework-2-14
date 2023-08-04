3bef53b51231ce840fe70d9195bac6ff386ffb6e
Evgenii.Pichugov <eugepic@yandex.ru>
2023-08-04 13:57:47 +0300

    Алгоритмы. Написал реализацию ArrayList, выполнил тесты

---
+++ src/main/java/org/example/Exception/FullArrayException.java
@@ -0,0 +1,7 @@
+package org.example.Exception;
+
+public class FullArrayException extends RuntimeException {
+    public FullArrayException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/Exception/IncorrectIndexException.java
@@ -0,0 +1,7 @@
+package org.example.Exception;
+
+public class IncorrectIndexException extends RuntimeException {
+    public IncorrectIndexException(String message) {
+        super(message);
+    }
+}
